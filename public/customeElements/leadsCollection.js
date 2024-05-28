55ae95c6da894a8e12b68fcf6023c4b122b67abb
kfirstri <kfirs@wix.com>
2024-05-28 16:11:17 +0300

    added new custome element

--- public/customeElements/leadsCollection.js
+++ public/customeElements/leadsCollection.js
@@ -1,3 +1,63 @@
+
+const elementStyle = `
+    @import url('https://fonts.googleapis.com/css2?family=Wix+Madefor+Text&display=swap');
+
+    .leads-collection-wrapper {
+        display: flex;
+        flex-direction: column;
+        
+        font-family: "Wix Madefor Text", sans-serif;
+        font-optical-sizing: auto;
+        font-weight: 400;
+        font-style: normal;
+    }
+
+    .form-wrapper {
+        display: flex;
+        gap: 10px;
+        align-items: flex-end;
+
+        span.submit-button {
+            background-color: blue;
+            color: white;
+            margin:0;
+            height: 46px;
+            width: 200px;
+            text-align: center;
+            vertical-align: middle;
+            line-height: 46px;
+        }
+    }
+
+    .email-input-wrapper {
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+
+        label {
+            font-weight: 300;
+        }
+
+        input {
+            height: 40px;
+            width: 300px;
+        }
+    }
+`;
+
+const htmlContent = `
+<span>
+    <h2>Leave your email to join our mailing list</h2>
+</span>
+<div class='form-wrapper'>
+    <div class="email-input-wrapper">
+        <label for="userEmail">Your email address *</label>
+        <input type="text" id="userEmail" />
+    </div>
+    <span class='submit-button'>Send</span>
+</div>
+`;
+
 class LeadsCollectionElement extends HTMLElement {
     constructor() {
         super();
@@ -8,21 +68,14 @@ class LeadsCollectionElement extends HTMLElement {
 
         const wrapper = document.createElement("div");
         wrapper.setAttribute("class", "leads-collection-wrapper");
-
-        const textEl = document.createElement("span");
-        textEl.textContent = 'Hello!'
+        wrapper.innerHTML = htmlContent;
 
         const style = document.createElement("style");
-        style.textContent = `
-            .leads-collection-wrapper {
-                border: 1px solid red;
-            }
-        `;
+        style.textContent = elementStyle;
 
         // Connect everything
         shadow.appendChild(style);
         shadow.appendChild(wrapper);
-        wrapper.appendChild(textEl);
     }
 };
 
