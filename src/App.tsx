713cbf0cb14018c2ffb1a06774bc527323a2d18a
kfirstri <kfirs@wix.com>
2024-05-28 11:18:36 +0300

    Added basic components

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button, Page, Table, WixDesignSystemProvider } from '@wix/design-system';
+import '@wix/design-system/styles.global.css';
+import './App.css';
+
+type LeadRecord = {
+  email: string,
+  status: 'SUBSCRIBED' | 'UNSUBSCRIBED',
+  validity: string,
+  subscribeDate: string
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const leads: LeadRecord[] = [
+    {
+      email: 'kfirs@wix.com',
+      status: 'SUBSCRIBED',
+      validity: 'VALID',
+      subscribeDate: '01/12/2024'
+    }
+  ];
+
+  const columns = [
+    {
+      title: 'Email',
+      render: (row: LeadRecord) => row.email
+    },
+    {
+      title: 'status',
+      render: (row: LeadRecord) => row.status
+    },
+    {
+      title: 'validity',
+      render: (row: LeadRecord) => row.validity
+    },
+    {
+      title: 'subscribeDate',
+      render: (row: LeadRecord) => row.subscribeDate
+    },
+  ];
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <WixDesignSystemProvider>
+      <Page>
+        <Page.Header title='Bla'
+          actionsBar={
+            <Button>Yo!</Button>
+          } />
+        <Page.Content>
+          <Table skin="standard" data={leads} columns={columns}>
+            <Table.Content />
+          </Table>
+        </Page.Content>
+      </Page>
+    </WixDesignSystemProvider>
   )
 }
 
