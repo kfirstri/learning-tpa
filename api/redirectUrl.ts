c98ed11461c5ec44715be90b57698511e39b7e1e
kfirstri <kfirs@wix.com>
2024-05-29 15:49:34 +0300

    add redirect to close window

--- api/redirectUrl.ts
+++ api/redirectUrl.ts
@@ -14,5 +14,29 @@ export async function GET(request: Request) {
     console.log('Saving code for instance id', instanceId);
     await kv.set(instanceId, code);
 
-    return new Response(`Success!`);
+    // Get access and refresh tokens
+    const response = await fetch(
+        'https://www.wixapis.com/oauth/access',
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                'grantType': 'authorization_code',
+                'clientId': process.env.APP_ID,
+                'clientSecret': process.env.APP_SECRET,
+                'code': code
+            })
+        }
+    )
+
+    const { access_token, refresh_token } = await response.json();
+
+    return new Response(null, {
+        status: 308,
+        headers: {
+            Location: `https://www.wix.com/installer/close-window?access_token=${access_token}`
+        }
+    });
 }
